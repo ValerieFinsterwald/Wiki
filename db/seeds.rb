1b13881d2c9104267f7dfdfa222637daa7a41809
Valerie Finsterwald <finsterwald.valerie@gmail.com>
2021-11-09 16:48:12 +0100

    Articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying articles"
+Article.destroy_all
+puts "creating articles"
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+  article.save
+end
+
+puts "#{Article.count} articles were created"
